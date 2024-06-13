1a18dd8a8d97bb3a3f79c9747ea1517f269765b9
Ana Flores <a01735115@tec.mx>
2024-06-13 12:06:56 -0600

    hello

--- lib/jswatch_web/statecharts/indiglo_manager.ex
+++ lib/jswatch_web/statecharts/indiglo_manager.ex
@@ -3,7 +3,7 @@ defmodule JswatchWeb.IndigloManager do
 
   def init(ui) do
     :gproc.reg({:p, :l, :ui_event})
-    {:ok, %{ui_pid: ui, st: IndigloOff, count: 0, timer1: nil}}
+    {:ok, %{ui_pid: ui, st: IndigloOff, count: 0, timer1: nil, snooze_timer: nil}}
   end
 
   def handle_info(:"top-right-pressed", %{ui_pid: pid, st: IndigloOff} = state) do
@@ -15,6 +15,7 @@ defmodule JswatchWeb.IndigloManager do
     timer = Process.send_after(self(), Waiting_IndigloOff, 2000)
     {:noreply, %{state | st: Waiting, timer1: timer}}
   end
+
   def handle_info(:"top-left-pressed", state) do
     :gproc.send({:p, :l, :ui_event}, :update_alarm)
     {:noreply, state}
@@ -22,7 +23,7 @@ defmodule JswatchWeb.IndigloManager do
 
   def handle_info(Waiting_IndigloOff, %{ui_pid: pid, st: Waiting} = state) do
     GenServer.cast(pid, :unset_indiglo)
-    {:noreply, %{state| st: IndigloOff}}
+    {:noreply, %{state | st: IndigloOff}}
   end
 
   def handle_info(:start_alarm, %{ui_pid: pid, st: IndigloOff} = state) do
@@ -42,10 +43,8 @@ defmodule JswatchWeb.IndigloManager do
     end
     GenServer.cast(pid, :unset_indiglo)
     Process.send_after(self(), AlarmOff_AlarmOn, 500)
-
-    {:noreply, %{state| count: 51, timer1: nil, st: AlarmOff}}
+    {:noreply, %{state | count: 51, timer1: nil, st: AlarmOff}}
   end
-
 
   def handle_info(AlarmOn_AlarmOff, %{ui_pid: pid, count: count, st: AlarmOn} = state) do
     if count >= 1 do
@@ -56,8 +55,8 @@ defmodule JswatchWeb.IndigloManager do
       GenServer.cast(pid, :unset_indiglo)
       {:noreply, %{state | count: 0, st: IndigloOff}}
     end
-
   end
+
   def handle_info(AlarmOff_AlarmOn, %{ui_pid: pid, count: count, st: AlarmOff} = state) do
     if count >= 1 do
       Process.send_after(self(), AlarmOn_AlarmOff, 500)
@@ -69,8 +68,36 @@ defmodule JswatchWeb.IndigloManager do
     end
   end
 
+  def handle_info(:"bottom-left-pressed", %{ui_pid: pid, st: st} = state) do
+    if st in [AlarmOn, AlarmOff] do
+      GenServer.cast(pid, :unset_indiglo)
+      if state.snooze_timer != nil do
+        Process.cancel_timer(state.snooze_timer)
+      end
+      {:noreply, %{state | count: 0, st: IndigloOff, snooze_timer: nil}}
+    else
+      {:noreply, state}
+    end
+  end
+
+  def handle_info(:snooze, %{ui_pid: pid, st: AlarmOff} = state) do
+    Process.send_after(self(), AlarmOn_AlarmOff, 500)
+    GenServer.cast(pid, :set_indiglo)
+    {:noreply, %{state | count: 51, st: AlarmOn}}
+  end
+
   def handle_info(event, state) do
     IO.inspect(event)
+    {:noreply, state}
+  end
+
+  def snooze_alarm(pid) do
+    snooze_timer = Process.send_after(self(), :snooze, 10 * 60 * 1000) # 10 minutos
+    {:noreply, %{pid | snooze_timer: snooze_timer}}
+  end
+
+  def handle_cast(:snooze_alarm, state) do
+    snooze_alarm(state.ui_pid)
     {:noreply, state}
   end
 end
